649311a37dca462123ff0d0ef411740cf5c9e384
majedalswaeer <majedalswaeer69@gmail.com>
2021-07-05 16:28:47 +0300

    done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 let leftEL = document.getElementById('leftimg');
 let middleEL = document.getElementById('middleimg');
 let rightEL = document.getElementById('rightimg');
+let butEL = document.getElementById('resultsClick');
+butEL.addEventListener('click' , getResults);
 
 
 let mallArr = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'];
@@ -10,6 +12,9 @@ let mallArr = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.
 let newnames = [];
 let att = 1;
 let maxAtt = 25;
+let chartNames = [];
+let votesChart =[];
+let viewsChart =[];
 
 //_____________________________________ Constructer
 function Busmall(imgNamee) {
@@ -17,6 +22,7 @@ function Busmall(imgNamee) {
   this.img = 'imgs/' + imgNamee;
   this.views = 0;
   this.votes = 0;
+  chartNames.push(this.name);
   newnames.push(this);
 }
 //_____________________________________end
@@ -53,16 +59,20 @@ function createImg() {
   middleIndex = randImg();
   rightIndex = randImg();
 
-  while (leftIndex === middleIndex || rightIndex === middleIndex) {
+  while (leftIndex === middleIndex || rightIndex === middleIndex || leftIndex=== rightIndex) {
     leftIndex = randImg();
     rightIndex = randImg();
   }
 
+  leftEL.setAttribute('src', newnames[leftIndex].img);
   leftEL.setAttribute('title', newnames[leftIndex].name);
 
-  middleEL.setAttribute('title', newnames[middleIndex].name);
 
-  rightEL.setAttribute('title', newnames[rightIndex].name);
+  middleEL.setAttribute('src', newnames[middleIndex].img);
+  middleEL.setAttribute('alt', newnames[middleIndex].name);
+
+  rightEL.setAttribute('src', newnames[rightIndex].img);
+  rightEL.setAttribute('alt', newnames[rightIndex].name);
 
   newnames[leftIndex].views++;
   newnames[middleIndex].views++;
@@ -75,15 +85,17 @@ createImg();
 //_____________________________________end
 
 //_____________________________________adding events
+let ulEL = document.getElementById('resultsContanier');
 leftEL.addEventListener('click', getClicks);
 rightEL.addEventListener('click', getClicks);
 middleEL.addEventListener('click', getClicks);
 
-let ulEL = document.getElementById('resultsContanier');
+
 
 function getClicks(event) {
   if (att <= maxAtt) {
     let clickedimg = event.target.id;
+    att++;
 
     createImg();
     if (clickedimg === 'leftimg') {
@@ -95,19 +107,74 @@ function getClicks(event) {
     } else if (clickedimg === 'rightimg') {
       newnames[rightIndex].votes++;
     }
-  } else {
-    for (let i = 0; i < newnames.length; i++) {
-      let liEL = document.createElement('li');
-      liEL.textContent = `${newnames[i].name}img has ${newnames[i].views} views and ${newnames[i].votes} votes`;
-      ulEL.appendChild(liEL);
-    }
-    leftEL.removeEventListener('click',getClicks);
-    middleEL.removeEventListener('click',getClicks);
-    rightEL.removeEventListener('click',getClicks);
 
   }
-  att++;
+  
+
 
 }
+
+
+
+function getResults(){
+  for (let i = 0; i < newnames.length; i++) {
+    let liEL = document.createElement('li');
+    liEL.textContent = `${newnames[i].name}img has ${newnames[i].views} views and ${newnames[i].votes} votes`;
+    ulEL.appendChild(liEL);
+    votesChart.push(newnames[i].votes);
+    viewsChart.push(newnames[i].views);
+  }
+
+  leftEL.removeEventListener('click',getClicks);
+  middleEL.removeEventListener('click',getClicks);
+  rightEL.removeEventListener('click',getClicks);
+  createImg();
+  majedChart();
+
+}
+
+//_____________________________________end
+
+
+//_____________________________________chart function
+function majedChart(){
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: chartNames,
+      datasets: [{
+        label: '# of Votes',
+        data: votesChart,
+        backgroundColor: [
+          'rgba(67, 23, 456, 123)',
+        ],
+        borderColor: [
+          'rgba(55, 19, 112, 31)',
+        ],
+        borderWidth: 1
+      },{
+        label: '# of Views',
+        data: viewsChart,
+        backgroundColor: [
+          'rgba(235, 39, 332, 3.2)',
+        ],
+        borderColor: [
+          'rgba(11, 19, 32, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+//_____________________________________end
+
 
 
